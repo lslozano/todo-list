975565e3bceccf17b4a3b5f71a78c3dc03bdf6d5
Luis Lozano <lslozano11@gmail.com>
2020-10-06 19:07:27 -0500

    Simple to do list done

---
+++ date.js
@@ -0,0 +1,22 @@
+exports.getDate = function() {
+  const date = new Date();
+  
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }
+  
+  return date.toLocaleDateString("en-US", options);
+}
+
+exports.getDay = function() {
+  const date = new Date();
+  
+  const options = {
+    weekday: "long"
+  }
+  
+  return date.toLocaleDateString("en-US", options);
+}
